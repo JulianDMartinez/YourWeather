fc5db28414627bf3956ad692140b29f8b0c2df33
Julian Martinez <julian.martinez.s@outlook.com>
2024-09-22 17:53:49 -0500

    Initial Commit

---
+++ YourWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YourWeather
+//
+//  Created by Julian Martinez on 9/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YourWeather/YourWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourWeatherApp.swift
+//  YourWeather
+//
+//  Created by Julian Martinez on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct YourWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
